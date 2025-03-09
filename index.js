0612332fb3703ebf36a060b52fd03621d44ded29
karponick <nickkarpo@gmail.com>
2025-03-09 13:52:34 -0400

    mostly finished

--- index.js
+++ index.js
@@ -1,86 +1,123 @@
-var channels = {};
+var channels = {}; // need to remove this
+var chanObjs = {};
 var selectedChannelNum = 0;
 var dropVis = false;
 
+class Channel {
+    constructor(id) {
+        this._id = id;
+        this.timerInterval = setInterval(function () {}, 1000);
+        this.time = 0;
+        // Create elements
+        this.channel = document.createElement("div");
+        this.delX = document.createElement("label");
+        this.chanText = document.createElement("label");
+        this.normalBut = document.createElement("div");
+        this.mutantSpawnBut = document.createElement("div");
+        this.mutantBut = document.createElement("div");
+        this.timer = document.createElement("label");
+        this.moveBut = document.createElement("label");
+    }
+
+    // Function to create elements for a channel
+    createChannel() {
+        // Set ids
+        this.setIds(this._id);
+
+        // Set class names
+        this.channel.className = "Channel";
+        this.delX.className = "Delete";
+        this.chanText.className = "ChanText";
+        this.normalBut.className = "Button";
+        this.mutantSpawnBut.className = "Button";
+        this.mutantBut.className = "Button";
+        this.timer.className = "Timer";
+        this.moveBut.className = "Move";
+
+        // Set texts
+        this.delX.textContent = "X";
+        this.chanText.textContent = "▾ Channel";
+        this.normalBut.textContent = "Normal";
+        this.mutantSpawnBut.textContent = "Lightning";
+        this.mutantBut.textContent = "Mutant";
+        this.timer.textContent = "0:00";
+        this.moveBut.textContent = "≡";
+
+        // Add children to channel
+        this.channel.appendChild(this.delX);
+        this.channel.appendChild(this.chanText);
+        this.channel.appendChild(this.normalBut);
+        this.channel.appendChild(this.mutantSpawnBut);
+        this.channel.appendChild(this.mutantBut);
+        this.channel.appendChild(this.timer);
+        this.channel.appendChild(this.moveBut);
+        getElement("Channel-Grid").appendChild(this.channel);
+
+        // Set onclick attributes
+        this.delX.addEventListener("click", () => {
+            deleteChannel(this._id, this.channel);
+        });
+        this.chanText.addEventListener("click", () => {
+            dropDown(this);
+        });
+        this.normalBut.addEventListener("click", () => {
+            this.time = 300;
+            setTimer(this);
+        });
+        this.mutantSpawnBut.addEventListener("click", () => {
+            this.time = 120;
+            setTimer(this);
+        });
+        this.mutantBut.addEventListener("click", () => {
+            this.time = 480;
+            setTimer(this);
+        });
+
+        dropDown(this); // Open drop down for channel select when creating new channel
+    }
+
+    setIds(id) {
+        this._id = id;
+        this.channel.id = "Channel" + id;
+        this.delX.id = "Delete" + id;
+        this.chanText.id = "Text" + id;
+        this.timer.id = "Timer" + id;
+        this.moveBut.id = "Move" + id;
+    }
+}
 // Add Channel Tab
 function addChannel() {
     try {
-        const currentTimeInMilliseconds = Date.now();
-        selectedChannelNum = currentTimeInMilliseconds;
-        let channel = document.createElement("div");
-        channel.id = "Channel" + selectedChannelNum;
-        channel.className = "Channel";
+        var chan = new Channel(Date.now()); // Current time in ms)
+        chan.createChannel();
+        chanObjs[chan._id] = chan;
 
-        let del = document.createElement("label");
-        let div1 = document.createElement("div");
-        let text = document.createElement("label");
-        let drop = document.createElement("label");
-        let normal = document.createElement("div");
-        let mutant = document.createElement("div");
-        let time = document.createElement("label");
-        let move = document.createElement("label");
-
-        del.id = "Delete" + selectedChannelNum;
-        del.className = "Delete";
-        del.textContent = "x";
-
-        text.id = "Text" + selectedChannelNum;
-        text.textContent = "Channel";
-
-        drop.textContent = "▾";
-        drop.id = "Drop" + selectedChannelNum;
-        drop.className = "Drop";
-        drop.setAttribute("onclick", `dropDown("${text.id}")`);
-
-        normal.className = "Button";
-        normal.textContent = "Normal";
-
-        mutant.className = "Button";
-        mutant.textContent = "Mutant";
-
-        time.textContent = "0:00";
-
-        move.className = "Move";
-        move.textContent = "≡";
-
-        div1.appendChild(text);
-        div1.appendChild(drop);
-        channel.appendChild(del);
-        channel.appendChild(div1);
-        channel.appendChild(normal);
-        channel.appendChild(mutant);
-        channel.appendChild(time);
-        channel.appendChild(move);
-
-        getElement("Channel-Grid").appendChild(channel);
-        dropVis = false;
-        dropDown(text.id);
-        del.setAttribute("onclick", `deleteChannel("Channel${selectedChannelNum}")`);
+        // dropVis = false;
     } catch (e) {
         console.log(e);
     }
 }
 
 // View/Hide Drop Down menu for channel number select
-function dropDown(textId) {
+function dropDown(channel) {
     let drop = getElement("DropDown");
-    selectedChannelNum = getElement(textId).parentElement.parentElement.id.substring(7);
-    var rect = getElement(textId).getBoundingClientRect();
+    selectedChannelNum = channel._id;
+    var rect = channel.channel.getBoundingClientRect();
     if (dropVis) {
-        if (drop.style.top != rect.bottom + 10 + "px") {
-            drop.style.top = rect.bottom + 10 + "px";
+        if (drop.style.top != rect.bottom + "px") {
+            drop.style.top = rect.bottom + "px";
         } else {
             hideDrop();
         }
     } else if (!dropVis) {
         dropVis = true;
-        drop.style.top = rect.bottom + 10 + "px";
-        drop.style.left = rect.left - 45 + "px";
+        drop.style.top = rect.bottom + "px";
+        drop.style.left = rect.left + "px";
 
         var children = drop.children;
         for (let index = 0; index < children.length; index++) {
             var child = children[index];
-            if (channels["Channel" + (index + 1)]) child.className = "DropChoiceTaken";
+            if (chanObjs[index + 1]) child.className = "DropChoiceTaken";
         }
 
         drop.style.visibility = "visible";
@@ -89,29 +126,22 @@ function dropDown(textId) {
 
 // Selecting channel number
 function dropSelect(num) {
-    // Channel
-    getElement("Channel" + selectedChannelNum).id = "Channel" + num;
-    // Delete
-    getElement("Delete" + selectedChannelNum).id = "Delete" + num;
-    getElement("Delete" + num).setAttribute("onclick", `deleteChannel("Channel${num}")`);
-    // Text
-    getElement("Text" + selectedChannelNum).id = "Text" + num;
-    getElement("Text" + num).textContent = "Channel " + num;
-    // Drop
-    getElement("Drop" + selectedChannelNum).id = "Drop" + num;
-    getElement("Drop" + num).setAttribute("onclick", `dropDown("Text${num}")`);
-    hideDrop();
-    clearDropNum(selectedChannelNum);
-    channels["Channel" + num] = true;
-    selectedChannelNum = num;
+    if (!chanObjs[num]) {
+        chanObjs[selectedChannelNum].setIds(num);
+        chanObjs[num] = chanObjs[selectedChannelNum];
+        delete chanObjs[selectedChannelNum];
+        chanObjs[num].chanText.textContent = "▾ Channel " + num;
+        hideDrop();
+        clearDropNum(selectedChannelNum);
+    }
 }
 
 // Remove Channel Tab
-function deleteChannel(channelId) {
-    console.log(channelId);
-    getElement("Channel-Grid").removeChild(getElement(channelId));
+function deleteChannel(channelNum, channel) {
+    getElement("Channel-Grid").removeChild(channel);
     hideDrop();
-    clearDropNum(channelId.substring(7));
+    delete chanObjs[channelNum];
+    clearDropNum(channelNum);
 }
 
 // Hides Drop Down menu for channel
@@ -123,13 +153,54 @@ function hideDrop() {
 // Clears selection for channel number
 function clearDropNum(num) {
     if (num < 31) {
-        channels["Channel" + num] = false;
         getElement("p" + num).className = "DropChoice";
     }
 }
 
+function setTimer(channel) {
+    clearInterval(channel.timerInterval);
+    var minutes = (minutes = Math.trunc(channel.time / 60));
+    var seconds = 0;
+    var secondsText = "00";
+    channel.timer.textContent = `${minutes}:${secondsText}`;
+    channel.timerInterval = setInterval(() => {
+        channel.time--;
+        minutes = Math.trunc(channel.time / 60);
+        seconds = channel.time - minutes * 60;
+        if (seconds < 10) {
+            secondsText = "0" + seconds;
+        } else {
+            secondsText = seconds.toString();
+        }
+
+        channel.timer.textContent = `${minutes}:${secondsText}`;
+        // Stop timer once it hits 0
+        if (channel.time == 0) {
+            clearInterval(channel.timerInterval);
+        }
+    }, 1000);
+}
+
 // Sort channels by Time Remaining OR Channel Number
-function sort() {}
+function sortByChannelNum() {
+    hideDrop();
+    for (let [num, chan] of Object.entries(chanObjs)) {
+        chan.channel.style.order = chan._id;
+    }
+}
+
+function sortByTime() {
+    hideDrop();
+    let times = [];
+    for (let [num, chan] of Object.entries(chanObjs)) {
+        let obj = { id: chan._id, time: chan.time };
+        times.push(obj);
+    }
+    times.sort((a, b) => a.time - b.time);
+    for (let index = 0; index < times.length; index++) {
+        chanObjs[times[index].id].channel.style.order = index;
+    }
+}
 
 // Shorter get elementbyid... but still kinda long idk why I did this
 function getElement(id) {
@@ -139,6 +210,9 @@ function getElement(id) {
 // Create Drop Down menu on load and add event listeners for adding channel and drop down selections
 window.onload = function () {
     getElement("Add").setAttribute("onclick", "addChannel()");
+    getElement("SortNum").setAttribute("onclick", "sortByChannelNum()");
+    getElement("SortTime").setAttribute("onclick", "sortByTime()");
+    window.addEventListener("scroll", hideDrop);
 
     var dropDiv = document.createElement("div");
     dropDiv.id = "DropDown";
